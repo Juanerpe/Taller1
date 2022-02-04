216ed8ab869b5738ba30c8bb53ffbe86aa7e00fd
Juanerpe <98862983+Juanerpe@users.noreply.github.com>
2022-02-04 00:00:45 -0500

    Add files via upload
    creacion de proyecto java, la clase main y la primera clase del taller, fruit

---
+++ JavaApplication1/src/Class/Fruit.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Class;
+
+/**
+ * una primera clase para observar lo aprendido en clase  
+ * @author juan erasmo perez alvarado
+ */
+public class Fruit {
+    /**
+     * Representa el nombre de la fruta
+     */
+    public String Name ;
+    /**
+     * Representa el peso de la fruta
+     */
+    public float averageWeight;
+     /**
+     * Representa la forma de la fruta
+     */
+    public String Shape;
+     /**
+     * Representa la especie o familia a la cual pertenece
+     */
+    public String genus;
+}
